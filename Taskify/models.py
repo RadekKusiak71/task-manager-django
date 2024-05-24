292f57e847f8766da1bee7883fad9e81d382cc54
RadekKusiak71 <radoslawkusiak12@gmail.com>
2024-05-24 12:21:28 +0200

    Created models + migrations

--- Taskify/models.py
+++ Taskify/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+
+def get_upload_path(instance, filename) -> str:
+    return f'{instance.user.username}/{filename}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to=get_upload_path, default='./static/images/default_avatar.jpg', validators=[
+                               FileExtensionValidator(['png', 'jpg', 'jpeg'])])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} #{self.id}'
+
+
+class Task(models.Model):
+    PRIORITY_STATUS = {
+        "low": "Low",
+        "medium": "Medium",
+        "high": "High"
+    }
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_STATUS)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Task#{self.id} - {self.title}'
